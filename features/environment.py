453090c11f6436f48e6ad258f2ce798448973606
Sebastian Leal <sebastian.leal@limelighthealth.com>
2021-03-20 21:50:54 -0500

    change options

--- features/environment.py
+++ features/environment.py
@@ -10,11 +10,12 @@ with open("test_data/config.json") as conf:
 
 def before_scenario(context, scenario):
     chrome_options = Options()
+    chrome_options.add_argument('headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--profile-directory=Default')
-    chrome_options.add_argument('--user-data-dir=~/.config/google-chrome')
+    chrome_options.add_argument('--remote-debugging-port=9222')
     context.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     context.driver.get(data["urls"]['base_url'])
 
