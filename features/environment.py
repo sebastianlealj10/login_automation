336d83b19b2f6afb5225b10ddb4c34b449192a30
Sebastian Leal <sebastian.leal@limelighthealth.com>
2021-03-20 21:46:56 -0500

    change option

--- features/environment.py
+++ features/environment.py
@@ -11,7 +11,6 @@ with open("test_data/config.json") as conf:
 def before_scenario(context, scenario):
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--profile-directory=Default')
