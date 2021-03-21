134c99b9e953bb900a6095c33dc22287aa320c6f
Sebastian Leal <sebastian.leal@limelighthealth.com>
2021-03-20 21:41:56 -0500

    add options

--- features/environment.py
+++ features/environment.py
@@ -2,13 +2,21 @@ import json
 
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 with open("test_data/config.json") as conf:
     data = json.load(conf)
 
 
 def before_scenario(context, scenario):
-    context.driver = webdriver.Chrome(ChromeDriverManager().install())
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--profile-directory=Default')
+    chrome_options.add_argument('--user-data-dir=~/.config/google-chrome')
+    context.driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     context.driver.get(data["urls"]['base_url'])
 
 
