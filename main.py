2a5b2be1c6821427c8b25e7518c6a1307d99b35a
Sebastian Leal <sebastian.leal@limelighthealth.com>
2021-03-20 22:37:49 -0500

    add reports

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from behave.__main__ import main as behave_main
 
 if __name__ == '__main__':
-    behave_main("-f allure_behave.formatter:AllureFormatter -o allure-results features/login.feature")
+    behave_main("features/login.feature")
